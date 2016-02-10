3e13526c4770f13e102d1f50dbdd0f88a12d60fd
B2F <d4rb2f@gmail.com>
2016-02-10 16:47:38 +0100

    Restructured the stat object, replacing csssConfig for parsing pages

--- csssc.js
+++ csssc.js
@@ -19,17 +19,15 @@ var stats = {
   },
 };
 /**
- * Validates csssConfig.pages, adds default values and required data:
+ * Prepare planned pages for parsing:
  *
  * 'name': the webpage's name reference.
  * 'url': the webpage's url used for taking screenshots.
  * 'captures': all screenshots captures for this webpage.
  */
-var validatePagesConfiguration = function() {
+var planPages = function() {
 
     Object.keys(csssConfig.pages).forEach(function(page) {
-
-        csssConfig.pages[page].name = page;
 
         if (csssConfig.pages[page].url == undefined) {
             throw '[ERROR] Your configuration page "' + page + '" requires an url."';
@@ -38,53 +36,51 @@ var validatePagesConfiguration = function() {
             csssConfig.pages[page].url = 'http://' + csssConfig.pages[page].url;
         }
 
-        if (csssConfig.pages[page].captures == undefined || !Object.keys(csssConfig.pages[page].captures).length) {
-            csssConfig.pages[page].captures = {
-                'body':''
-            }
-        }
-
-        var capturesConfig = csssConfig.pages[page].captures;
-
         // Pages and captures may be limited by arguments in future builds, hence recording planned pages object separately.
         stats.planned.pages[page] = {
             url: csssConfig.pages[page].url,
-            captures: []
+            captures: {}
         };
 
+        var capturesConfig = {'body': ''}
+
+        if (csssConfig.pages[page].captures != undefined && Object.keys(csssConfig.pages[page].captures).length) {
+            capturesConfig = csssConfig.pages[page].captures;
+        }
+
         for (var c in capturesConfig) {
-            csssConfig.pages[page].captures[c] = {
+            stats.planned.pages[page].captures[c] = {
                 selector:capturesConfig[c] || c
             }
-            stats.planned.pages[page].captures.push(c);
         }
     });
 }
 /**
- * Validates csssConfig.viewports configuration:
+ * Prepare planned viewports for parsing:
  *
  * 'name': the viewports name.
  * 'height': the viewports height.
  * 'width': the viewports width.
  */
-var validateViewportsConfiguration = function() {
+var planViewports = function() {
 
-    if (!csssConfig.viewports || typeof(csssConfig.viewports) != 'object') {
-        csssConfig.viewports = {
-            'default': {
-                'width':1366,
-                'height':768
-            }
+    stats.planned.viewports = {
+        'default': {
+            'width':1366,
+            'height':768
         }
     }
 
-    Object.keys(csssConfig.viewports).forEach(function(viewportName) {
-      csssConfig.viewports[viewportName].name = viewportName;
-      if (typeof csssConfig.viewports[viewportName].height != 'number' ||
-          typeof csssConfig.viewports[viewportName].width != 'number') {
-        throw "[ERROR] Viewport height and width must be set with numbers.";
-      }
-    });
+    if (csssConfig.viewports || typeof(csssConfig.viewports) == 'object') {
+        stats.planned.viewports = {};
+        Object.keys(csssConfig.viewports).forEach(function(viewportName) {
+            stats.planned.viewports[viewportName] = csssConfig.viewports[viewportName];
+            if (typeof csssConfig.viewports[viewportName].height != 'number' ||
+                typeof csssConfig.viewports[viewportName].width != 'number') {
+                throw "[ERROR] Viewport height and width must be set with numbers.";
+            }
+        });
+    }
 }
 
 /**
@@ -95,14 +91,12 @@ var validateViewportsConfiguration = function() {
  */
 module.exports = {
 
-    initConfiguration: function(configuration) {
+    init: function(configuration) {
         csssConfig = configuration;
-        validateViewportsConfiguration();
-        validatePagesConfiguration();
+        planViewports();
+        planPages();
     },
-    getConfiguration: function() {
-        return csssConfig;
-    },
+
     getStats: function() {
         return stats;
     },
--- tests/suite.js
+++ tests/suite.js
@@ -4,20 +4,19 @@ var assert          = require('assert'),
 
 describe('CSSsc configuration', function() {
 
-    csssc.initConfiguration(fullPageConfig);
-    var csssconfig = csssc.getConfiguration();
+    csssc.init(fullPageConfig);
+    var stats = csssc.getStats();
 
     it('should generate stats', function() {
-        var stats = csssc.getStats();
         assert.notStrictEqual(stats.planned.pages, Object.keys(fullPageConfig.pages));
     });
 
     it('should have default body selector', function() {
-        assert.equal(csssconfig.pages.bodyDefault.captures.body.selector, 'body');
+        assert.equal(stats.planned.pages.bodyDefault.captures.body.selector, 'body');
     });
 
     it('should have named selectors', function() {
-        assert.equal(csssconfig.pages.bodyCapture.captures.fullPage.selector, 'body');
+        assert.equal(stats.planned.pages.bodyCapture.captures.fullPage.selector, 'body');
     });
 
     it('should define a default client driver', function() {
@@ -25,7 +24,7 @@ describe('CSSsc configuration', function() {
     });
 
     it('should define a default viewport', function() {
-        assert.ok(csssconfig.viewports.default.width);
+        assert.ok(stats.planned.viewports.default.width);
     });
 })
 
